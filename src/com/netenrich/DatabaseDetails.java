6af47342e1322452cb33dfea4efecfb02f4eae15
kishore38 <kishorekumarnetala@gmail.com>
2017-02-02 11:02:20 +0530

    added ip

--- src/com/netenrich/DatabaseDetails.java
+++ src/com/netenrich/DatabaseDetails.java
@@ -1,7 +1,7 @@
 package com.netenrich;
 
 public class DatabaseDetails {
-	private String database_url = "jdbc:mysql://localhost/register";
+	private String database_url = "jdbc:mysql://35.154.148.219:3306/register";
 	private String database_username = "root";
 	private String database_password = "Pass@123";
 	public String Databaseurl(){
